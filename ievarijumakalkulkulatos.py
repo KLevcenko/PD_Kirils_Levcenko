cc68983737360a899e91308cd1b0f932198e7294
KLevcenko <levcenkok16@gmail.com>
2026-03-04 14:34:30 +0200

    mainiga nosakuma izmainišana un aprekini

--- ievarijumakalkulkulatos.py
+++ ievarijumakalkulkulatos.py
@@ -1,5 +1,7 @@
 def ieavde():
     abolu_svars=float(input("Ābolu svars(kg): "  ))
-    cukura_svars=float(input("Cukuru svars: "))
-def aprekin():
+    cukura_cena=float(input("Cukuru svars: "))
+def aprekin(abolu_svars, cukura_cena):
+  cukura_svars=abolu_svars/2
+  kopejas_izmaksas=cukura_cena*cukura_svars
 def izvade():
